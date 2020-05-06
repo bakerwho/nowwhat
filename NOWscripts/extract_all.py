c76be39f0188237effd01aee0cb5363dd3a0c001
bakerwho <aabir22@gmail.com>
2020-05-06 14:58:52 -0400

    script to extract ZIPfiles

---
+++ NOWscripts/extract_all.py
@@ -0,0 +1,74 @@
+import os
+from os.path import join
+from zipfile import ZipFile
+import string
+import re
+from collections import Counter, defaultdict
+
+NOWfolder = '/project2/jevans/Davies_corpora/NOW/'
+datafolder = '/project2/jevans/aabir/NOWwhat/NOWdata'
+
+files = os.listdir(NOWfolder)
+anomalies = [f for f in NOWfiles if len(re.findall(r'\d+', f))==1]
+for an in anomalies:
+    files.remove(an)
+
+numfiles = len(NOWfiles)
+
+def files_by_year(files):
+    years = {file: tuple(int(i) for i in re.findall(r'\d+', file)) for file in files}
+    return years
+
+years = Counter(list(files_by_year(files).values()))
+print(years)
+
+def files_by_datatype(files):
+    files_by_dt = defaultdict(list)
+    dts = ['lexicon', 'db', 'text', 'wlp', 'sources']
+    for file in files:
+        for dt in dts:
+            if dt in file:
+                files_by_dt[dt].append(file)
+    return files_by_dt
+
+files_by_dt = files_by_datatype(files)
+print({k:len(v) for k, v in files_by_dt.items()})
+
+def unzip_files(filenames, sourcepath, destpath, condition=lambda x: True):
+    successes = 0
+    tries = 0
+    for file in filenames:
+        if not condition(file):
+            continue
+        print(f'unzipping file {file}')
+        tries += 1
+        try:
+            ym = tuple(re.findall(r'\d+', file))
+            assert len(ym) == 2
+            y, m = ym
+            date = f"{y}-{m}"
+            spath = join(sourcepath, file)
+            dpath = join(destpath, year)
+            os.makedirs(dpath, exist_ok = True)
+        except:
+            print("folder creation Exception")
+            dpath = destpath
+        try:
+            with ZipFile('sampleDir.zip', 'r') as zipObj:
+                zipObj.extractall(dpath)
+            successes += 1
+        except Exception as e:
+            print(f"Unzipping exception: {e}")
+    print(f"Out of {len(filenames)} files: tried {tries} and \
+            extracted {successes}")
+
+unzip_files(files_by_dt['db'], NOWfolder, datafolder)
+unzip_files(files_by_dt['sources'], NOWfolder, datafolder)
+unzip_files(anomalies, NOWfolder, datafolder)
+
+
+
+
+
+
+# unzip -o /project2/jevans/Davies_corpora/NOW/db_16-10-ykw.zip -d /project2/jevans/aabir/NOWwhat/NOWdata/16-10/
