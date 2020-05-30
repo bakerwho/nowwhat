2d2a051406dd53fff453f3300c9c87feee4acf4b
bakerwho <aabir22@gmail.com>
2020-05-30 03:09:45 -0400

    [

--- exe/sentiment.sh
+++ exe/sentiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=aabir-lda
-#SBATCH --output=/project2/jevans/aabir/NOWwhat/logs/lda.out
-#SBATCH --error=/project2/jevans/aabir/NOWwhat/logs/lda.err
+#SBATCH --job-name=aabir-sentiment
+#SBATCH --output=/project2/jevans/aabir/NOWwhat/logs/sentiment.out
+#SBATCH --error=/project2/jevans/aabir/NOWwhat/logs/sentiment.err
 #SBATCH --partition=broadwl
 #SBATCH --mem=31GB
 #SBATCH --time=32:00:00
